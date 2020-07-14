0592f57a162e78551eb75ea02e0d844ba00d1c85
Monica Bustamante <lilianalambda@gmail.com>
2020-07-13 17:41:34 -0700

    Data Struture solution

--- class/class/e.py
+++ class/class/e.py
@@ -138,3 +138,47 @@ class LinkedListQueue(AbstractQueue):
         if self.is_empty():
             raise IndexError("Queue is empty")
         return self._front.value
+
+    def __init__(self, limit = 10):
+        self.queue = []
+        self.front = None
+        self.rear = None
+        self.limit = limit
+        self.size = 0
+        # self.storage = ?
+    
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        return ' '.join([str(i) for i in self.queue])
+    
+    def isEmpty(self):  
+        return self.size <= 0
+    
+    def enqueue(self, value): # to add rear end of the queue (poner cola)
+        if self.size >= self.limit:
+            return -1          #overflow
+        else:
+            self.queue.append(value)
+                
+        if self.front is None:  #the rear as size of the queue and front as 0
+            self.front = self.rear = 0
+        else:
+            self.rear = self.size
+            
+        self.size += 1
+        
+    def dequeue(self): # to pop, front end of the queue (remoder cola)
+        if self.isEmpty():
+            return -1          #underflow
+        else:
+            self.queue.pop()
+            self.size -= 1
+            if self.size == 0:
+                self.front = self.rear = 0
+            else:
+                self.rear = self.size - 1
+    
+    def getSize(self):
+        return self.size
--- queue/queue.py
+++ queue/queue.py
@@ -14,47 +14,17 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 
-class Queue(object): 
-    def __init__(self, limit = 10):
+class Queue(object):
+    def __init__(self):
         self.queue = []
-        self.front = None
-        self.rear = None
-        self.limit = limit
-        self.size = 0
-        # self.storage = ?
-    
-    def __len__(self):
-        return self.size
 
-    def __str__(self):
-        return ' '.join([str(i) for i in self.queue])
-    
-    def isEmpty(self):  
-        return self.size <= 0
-    
-    def enqueue(self, value): # to add rear end of the queue (poner cola)
-        if self.size >= self.limit:
-            return -1          #overflow
-        else:
-            self.queue.append(value)
-                
-        if self.front is None:  #the rear as size of the queue and front as 0
-            self.front = self.rear = 0
-        else:
-            self.rear = self.size
-            
-        self.size += 1
-        
-    def dequeue(self): # to pop, front end of the queue (remoder cola)
-        if self.isEmpty():
-            return -1          #underflow
-        else:
-            self.queue.pop()
-            self.size -= 1
-            if self.size == 0:
-                self.front = self.rear = 0
-            else:
-                self.rear = self.size - 1
-    
-    def getSize(self):
-        return self.size
+    def __len__(self):
+        return len(self.queue)
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if self.queue.__len__ < 1:
+            return None
+        return self.dequeue.pop()
